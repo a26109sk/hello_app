e0011a714677857ac5d2e2b6ad12a014b48317b3
a26109sk <a26109sk@aiit.ac.jp>
2026-04-13 11:43:07 +0000

    Update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   def hello
     render html: "hello, world! -> hola, mundo! -> ¡Hola, mundo!"
   end
+  
   def goodbye
     render html: "goodbye, world!"
   end
